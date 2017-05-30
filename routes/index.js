873fc7cc5c2890199536f4b9c4d60ed3ca4512ce
Rosa <sandorosa1@gmail.com>
2017-05-29 19:08:31 -0600

    ya hecho

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/datos', function(req, res, next){
+  var datos = [
+
+              {"Nombre": "Alexei"},
+              {"Apellido": "Rodriguez"},
+              {"Edad": 22},
+              {"Telefono": 94833503},
+
+  ];
+
+  res.json(datos);
+});
+
+router.get('/mensaje', function(req, res, next){
+  res.render('mensaje', {"txtMensaje":""});
+});
+
+var mensajes = [];
+
+router.post('/mensaje',function(req,res,next){
+
+    mensajes.push("prueba ".concat(req.body.txtMensaje));
+
+    var msgs = mensajes;
+    var rtObject = {};
+    rtObject.txtMensaje = req.body.txtMensaje;
+    rtObject.msg= msgs;
+  res.render('mensaje', rtObject);
+});
+
 module.exports = router;
